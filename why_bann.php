65fc4db5add47fab7da09767d637c758b2f04bcf
jadegung <jadegung@gmail.com>
2015-12-05 15:50:36 -0500

    Create why_bann.php

---
+++ why_bann.php
@@ -0,0 +1,49 @@
+<table align="center" cellpadding="0" cellspacing="0">
+   <tr>
+    
+      <!----- LOGO //----->   
+      <td>
+      <!--<h1 style="text-align:center; color:green;">Yes We CAN! Food Drives</h1>//-->
+      <a href="main.php"><img src="images/why_logo.GIF" alt="Wing Hong Yip Dragon Style Kung Fu Club"></a>
+      </td>
+
+
+      <!----- AD SPACE? //----->   
+      
+   </tr>
+</table>
+
+<!--
+<table width="100%" style="border-top:solid black; border-bottom:solid black; border-width:thin;" cellspacing="0" cellpadding="0">
+   <tr>
+      <td width="25%" height="30" valign="middle">
+      <div align="center"><font face="Helvetica" size="2" style="font-weight:bold;">&nbsp;</font></div>
+      </td>
+  
+
+     <td width="50%" height="30" valign="middle">
+     <div align="center"><font face="Helvetica" size="2" style="font-weight:bold;">&nbsp;</font></div> 
+     </td>
+
+     <td width="25%" align="right" height="30" valign="middle"> 
+     <div align="center"><font face="Helvetica" size="2" style="font-weight:bold;">&nbsp;</font></div>
+     </td>
+  </tr>
+</table>	 
+//-->
+
+<!-- <div style="border-top:solid black; border-bottom: solid black; border-width:thin;"> //-->
+<ul style="text-align:center; margin:9px 0 9px 0;">
+
+<li class="off"><a href="kung_fu.php">Kung Fu</a></li>
+<li class="off"><a href="tai_chi.php">Tai Chi</a></li>
+<li class="off"><a href="lion_dance.php">Lion Dance</a></li>
+<li class="off"><a href="https://www.youtube.com/user/NYCDragonStyleKungFu" target="blank">Videos</a></li>
+<li class="off"><a href="2014.php">Gallery</a></li>
+<li class="off"><a href="directions.php">Directions</a></li>
+
+<!-- <div style="margin-left:3em;margin-right:3em;"><hr></div> //-->
+ 
+</ul>
+
+<!-- </div> //-->
