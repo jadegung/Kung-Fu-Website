f1d11d1615b0305a07eedabc74f42ed5f3bbbde4
jadegung <jadegung@gmail.com>
2015-12-05 15:47:31 -0500

    Create kung_fu.php

---
+++ kung_fu.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>Welcome to Wing Hong Yip's Dragon Style Kung Fu and Tai Chi Club!</title>
+
+
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<style type="text/css">
+<!--
+.Red {
+	color: #F00;
+}
+
+.yellow {
+	color: #FF0;
+}
+-->
+</style>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="This image is copyright protected.";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+//-->
+</script>
+
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+
+</head>
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<body>
+<div class="container">
+
+<!---- LOGO AND ADVERTISEMENTS BEGIN HERE //----->
+<?php include("fixed/why_bann.php"); ?>
+
+</div>
+
+<div class="about">
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="20" leftmargin="30" rightmargin="30">
+
+<td height="135" valign="top" nowrap="nowrap" align="middle">
+<strong><center>
+  <h2 class="yellow"> Dragon Style Kung Fu </h2></center></strong>
+<p>Dragon Style Kung Fu was one of the five original systems of Chinese boxing that originated from the Shaolin Temple. Lam Yew Kwai was the first member of the public to learn this esoteric art from Monk Tai Yuk Sim See during the late 1800s.  Grandmaster Lam taught the system to individuals throughout China and in British Hong Kong. One of his students is Cho Sam who studied both Bak Mei (White Eyebrow) and Dragon Style Kung Fu. Grandmaster Cho would teach the complete Dragon Style Kung Fu system to Sifu Yip Wing Hong. Sifu Yip is one of the first individuals to spread the art in the United States.</p>
+<p>Dragon Style Kung Fu is a defensive and offensive combat system utilizing various hand techniques and low   kicks that strikes and traps the opponent. The dragon stylist employs a series of swift footwork, punches, grabs, locks, open handed strikes and kicks. The dragon stylist does not use brute force and muscular strength but develops sound waist power and applies similar Tai Chi principles.</p>
+<p>Training in Dragon Style Kung Fu is rigorous but rewarding. It involves developing powerful strikes and an awareness of the entire body for swiftness in defending and countering a sudden attack. Advancement is based solely on individual merit. There are no colored sashes or upgrading tests. Since Dragon Style Kung Fu is rarely taught in North America, few have have seen this rare blend of both external and internal powers. </p>
+  <p>&nbsp; </p>
+<center>
+<a href="Kung_Fu.JPG" rel="lightbox" title="Dragon Style Yellow Dragon Piercing Pole"><img src="Kung_Fu.JPG"></a>
+</center>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer">
+
+<!---- FOOTER BEGINS HERE //----->
+
+<?php include("fixed/whyfoot.php");?>
+
+<!----- FOOTER ENDS HERE //----->
+
+</div>
+</body>
+</html>
