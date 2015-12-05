46fff5f093d306b715993adc0fb0cc73d0b346d9
jadegung <jadegung@gmail.com>
2015-12-05 15:49:29 -0500

    Create 2015.php

---
+++ 2015.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>Welcome to Wing Hong Yip's Dragon Style Kung Fu and Tai Chi Club!</title>
+
+
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<style type="text/css">
+<!--
+.Red {
+	color: #F00;
+}
+.yellow {
+	color: #FF0;
+}
+-->
+</style>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="This image is copyright protected.";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+//-->
+</script>
+
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+
+</head>
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<body>
+<div class="container">
+
+<!---- LOGO AND ADVERTISEMENTS BEGIN HERE //----->
+<?php include("fixed/why_bann.php"); ?>
+
+</div>
+
+<div class="about">
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="20" leftmargin="30" rightmargin="30">
+<td height="135" valign="top" nowrap="nowrap" align="middle"><p>&nbsp;</p>
+<a href="videos.php"><center><p><h3><strong>Videos</strong></h3></p></center></a>
+  <center><h3><strong>Gallery</h3></center>
+<center>
+<span class="yellow"><strong><a href="Class_Training.php">Class Training</a> | <a href="2012.php">2012</a> | <a href="2013.php">2013</a> | <a href="2014.php">2014</a></strong></span>
+</center>
+<p><center>
+  <h2 class="yellow"><strong>2015 Photographs</strong></h2></center></p>
+<p><center><strong>Please click on the thumbnails to enlarge the photographs</strong></center></p>
+<center>
+<p><strong>August 2, 2015, International Day of Friendship</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/Bk1.jpg" rel="lightbox[Friendship]" title="Performing at Cadman Plaza Park."><img src="images/Bk1t.jpg" width="155" height="160" /></a><a href="images/Bk2.jpg" rel="lightbox[Friendship]" title="Leading the parade for Brooklyn Borough President Eric Adams."><img src="images/Bk2t.jpg" width="155" height="160" /></a></center></div>
+<center>
+<p><strong>April 23, 2015 Chinese American Insurance Association at Terrace on the Park</strong></p>
+<div class"thumbnail">
+<center>
+<a href="images/CAIA1.JPG" rel="lightbox[CA]" title="At Terrace on the Park in Corona, Queens."><img src="images/CAIA1t.jpg" width="155" height="160" /></a><a href="images/CAIA2.JPG" rel="lightbox[CA]" title="Leading the openining of the celebrations."><img src="images/CAIA2t.jpg" width="155" height="160" /></a><a href="images/CAIA3.JPG" rel="lightbox[CA]" title="Chasing each other's lion tail."><img src="images/CAIA3t.jpg" width="155" height="160" /></a></center></div>
+<center>
+<p><strong>March 5, 2015, Tourneau</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/Tourneau_1.jpg" rel="lightbox[Tourneau]" title="The drummer striking a pose during the performance."><img src="images/Tourneau_1t.jpg" width="155" height="160" /></a><a href="images/Tourneau_2.jpg" rel="lightbox[Tourneau]" title="Approaching the lettuce to satisfy the lion's hunger."><img src="images/Tourneau_2t.jpg" width="155" height="160" /></a></center></div>
+<p><strong>February 18, 2015, Time, Inc.</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/Time15_1.jpg" rel="lightbox[Time]" title="Greeting Time employees at its headquarters."><img src="images/Time15_1t.jpg" width="155" height="160" /></a><a href="images/Time15_2.jpg" rel="lightbox[Time]" title="The lion waking up to the sound of the drum, cymbals and gong."><img src="images/Time15_2t.jpg" width="155" height="160" /></a><a href="images/Time15_3.jpg" rel="lightbox[Time]" title="Time employees sharing a laugh."><img src="images/Time15_3t.jpg" width="155" height="160" /></a></center></div>
+<p><strong>February 11, 2015, The Chapin School</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/Chapin1.jpg" rel="lightbox[Chapin]" title="The monk interacts with the children by waving the fan towards the lion."><img src="images/Chapin1t.jpg" width="155" height="160" /></a><a href="images/Chapin2.jpg" rel="lightbox[Chapin]" title="The audience's enjoyment of the lion dance performance at the school gymnasium."><img src="images/Chapin2t.jpg" width="155" height="160" /><a href="images/Chapin3.jpg" rel="lightbox[Chapin]" title="The audience's enjoyment of the lion dance performance at the school gymnasium."><img src="images/Chapin3t.jpg" width="155" height="160" /></a></center></div>
+<p><strong>February 9, 2015, Wells Fargo Bank</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/Wells1.jpg" rel="lightbox[Wells]" title="Making a surprise appearance during the middle of the Chinese New Year event."><img src="images/Wells1t.jpg" width="155" height="160" /></a><a href="images/Wells2.jpg" rel="lightbox[Wells]" title="The lion is preparing to deliver a platter of food and good luck to an honorary guest."><img src="images/Wells2t.jpg" width="155" height="160" /></a></center></div>
+<p><strong>January 23, 2015, Friends Seminary Chinese New Year Celebration</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/FS15_1.JPG" rel="lightbox[FS]" title="Students of Friends Seminary observing the musicians' beats."><img src="images/FS15_1t.jpg" width="155" height="160" /></a><a href="images/FS15_2.JPG" rel="lightbox[FS]" title="Students of Friends Seminary feeding the lion."><img src="images/FS15_2t.jpg" width="155" height="160" /></a></center></div>
+<p><strong>January 20, 2015, Chinese Hand Laundry Alliance</strong></p> 
+<div class="thumbnail">
+<center>
+<a href="images/CHLA1.jpg" rel="lightbox[CHLA]" title="Feeding the lion."><img src="images/CHLA1t.jpg" width="155" height="160" /></a><a href="images/CHLA2.jpg" rel="lightbox[CHLA]" title="Giving the tangerine for good luck."><img src="images/CHLA2t.jpg" width="155" height="160" /></a></center></div>
+
+
+
+
+<p>&nbsp;</p>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer">
+
+<!---- FOOTER BEGINS HERE //----->
+
+<?php include("fixed/whyfoot.php");?>
+
+<!----- FOOTER ENDS HERE //----->
+
+</div>
+</body>
+</html>
