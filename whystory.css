0bb888f3d044af279c060a8884afdec525f90cee
jadegung <jadegung@gmail.com>
2015-12-05 15:51:44 -0500

    Create whystory.css

---
+++ whystory.css
@@ -0,0 +1,15 @@
+<hr>
+
+<table width="100%"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+     <td>
+   <div class="noindent" style="text-align:left;">
+   Questions, comments? Email to <a href="mailto:info@yipsdragonstylekungfu.com" target="_blank">
+ info@yipsdragonstylekungfu.com</a>.
+     </td>
+     <td style="text-align:right;"> <div class="noindent">Copyright&copy; 2015.</div>
+     </td>
+     
+   </tr>
+</table>
+<br>
