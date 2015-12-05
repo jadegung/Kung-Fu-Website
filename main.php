5da0681a5a5cf44db3aaf48f0ad3fee3cf634ed7
jadegung <jadegung@gmail.com>
2015-12-05 15:46:02 -0500

    Create main.php

---
+++ main.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>Welcome to Wing Hong Yip's Dragon Style Kung Fu and Tai Chi Club!</title>
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<style type="text/css">
+<!--
+.Red {
+	color: #F00;
+}
+-->
+</style>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="This image is copyright protected.";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+//-->
+</script>
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+</head>
+
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<body>
+<div class="container">
+
+<!---- LOGO AND ADVERTISEMENTS BEGIN HERE //----->
+<?php include("fixed/why_bann.php"); ?>
+
+</div>
+
+<div class="about">
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="20" leftmargin="30" rightmargin="30">
+<p> Since 1974, hidden on the outskirts of Chinatown, one can find Sifu Yip patiently teaching his group of dedicated students. Sifu Yip became the first instructor to teach Dragon Style Kung Fu publicly in the New York City area. He is also one of the few instructors teaching Dragon Style Kung Fu and Wu Style Tai Chi in North America. In addition, he studied briefly the White Eyebrow (Bak Mei) Kung Fu System; a sister system of Dragon Style Kung Fu. He has been teaching for over twenty-five years and has taught several thousands of students. Mr. Yip produced two Dragon Style Kung Fu videos in collaboration with ESPY-TV martial art videos and his Club appeared in Celebrate With Me: A Chinese New Year produced by Silver Burdett &amp; Ginn in 1991. </p>
+<center>
+<a href="images/Chili_Crab.jpg" rel="lightbox" title="Sifu Yip performing at the Singapore Chili Crab Festival in Long Island City."><img src="images/Chili_Crab.jpg" width="300" height="260" /></a>
+</center>
+<p>&nbsp;</p>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer">
+
+<!---- FOOTER BEGINS HERE //----->
+
+<?php include("fixed/whyfoot.php");?>
+
+<!----- FOOTER ENDS HERE //----->
+
+</div>
+</body>
+</html>
