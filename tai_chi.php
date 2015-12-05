e2c32c65dce6c59b40950313b6cb0c7daf721df7
jadegung <jadegung@gmail.com>
2015-12-05 15:52:24 -0500

    Create tai_chi.php

---
+++ tai_chi.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>Welcome to Wing Hong Yip's Dragon Style Kung Fu and Tai Chi Club!</title>
+
+
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<style type="text/css">
+<!--
+.Red {
+	color: #F00;
+}
+.yellow {
+	color: #FF0;
+}
+
+-->
+</style>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="This image is copyright protected.";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+//-->
+</script>
+
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+
+</head>
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<body>
+<div class="container">
+
+<!---- LOGO AND ADVERTISEMENTS BEGIN HERE //----->
+<?php include("fixed/why_bann.php"); ?>
+
+</div>
+
+<div class="about">
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="20" leftmargin="30" rightmargin="30">
+<td height="135" valign="top" nowrap="nowrap" align="middle"><strong><center>
+  <h2 class="yellow"> Wu Style Tai Chi </h2></center></strong><p>Tai Chi Chuan is an increasingly studied art throughout the world.  Individuals practice tai chi as a form of physical fitness to improve balance, increase stamina and improve flexibility.  The Dragon Style Kung Fu and Tai Chi Club integrates Dragon Style Kung Fu training with Wu Style Tai Chi.  Both systems share similarities and complement each other. For example, while Dragon Style generates both  external and internal power, Wu Style Tai Chi uses light and controlled movements to redirect an attack.</p>
+  <p>The core of training is the traditional 108-movement long form of Southern Wu Style Tai Chi coupled with push hands.  Wu Chuan Yau founded Wu Style Tai Chi after studying Yang Style Tai Chi.  His son, Wu Kam Chin, further developed the system so that it incorporates locks, throws, blocks and low kicks.  Students learn the 24-movement competitive short tai chi form.</p>
+<center>
+<a href="images/tai_chi.JPG" rel="lightbox" title="Sigung Ho Lai Performing Tai Chi at Flushing Meadows Park."><img src="images/tai_chi.jpg" width="155" height="160 /></a>
+</center>
+
+
+
+
+</div>
+
+
+
+
+<div class="footer">
+
+<!---- FOOTER BEGINS HERE //----->
+
+<?php include("fixed/whyfoot.php");?>
+
+<!----- FOOTER ENDS HERE //----->
+
+</div>
+</body>
+</html>
