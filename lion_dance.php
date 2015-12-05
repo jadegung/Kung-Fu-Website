a511bc5b5fe49ec9fb23efffa0f6cf6ae50fb62b
jadegung <jadegung@gmail.com>
2015-12-05 15:52:59 -0500

    Create lion_dance.php

---
+++ lion_dance.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>Welcome to Wing Hong Yip's Dragon Style Kung Fu and Tai Chi Club!</title>
+
+
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+<style type="text/css">
+<!--
+.Red {
+	color: #F00;
+}
+.yellow {
+	color: #FF0;
+}
+-->
+</style>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="This image is copyright protected.";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+//-->
+</script>
+
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects,builder"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+
+</head>
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<body>
+<div class="container">
+
+<!---- LOGO AND ADVERTISEMENTS BEGIN HERE //----->
+<?php include("fixed/why_bann.php"); ?>
+
+</div>
+
+<div class="about">
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="20" leftmargin="30" rightmargin="30">
+<td height="135" valign="top" nowrap="nowrap" align="middle">
+<strong><center>
+  <h2 class="yellow"> Southern Lion Dance </h2></center></strong><p>The lion dance is a rich traditional part of the Chinese heritage. The lion dance has strong ties with the traditions of the kung fu school. The lion dance requires many of the same attributes as for kung fu practitioners; upper body strength, a strong back to perform the lion tail, and the stamina required to maneuver a lion. The musicians (instruments include the drum, gong, and cymbals) must perform music that reflects closely to the movements and emotions of the lion. The lion dance is always present during   Chinese New Year where the lion can be found prancing in Chinatown. The lion dance serves broader functions that include corporate events and for public relations. Grand opening of businesses use lion dances to bring good luck, prosperity and visibility to its businesses. </p>
+  <p>
+The Club has performed lion dances that include Time, Inc., Tourneau, the Ms. Universe function held in New York City, the Coca-Cola centennial festivities in Atlanta, Georgia, the Saturday Morning Show, Bloomingdales, MetLife, HSBC Bank USA, Scholastic Book Publishing, Johnson &amp; Johnson, Friends Seminary, Southern Connecticut State University, Wesleyan University and Columbia University.  Kindly contact us via email below if you are interested in having a lion dance for your event.</p>
+<center><a href="images/lion_dance.JPG" rel="lightbox" title="Lion dance for a wedding banquet."><img src="images/lion_dance.JPG" width="150" height="160></a>
+</center>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer">
+
+<!---- FOOTER BEGINS HERE //----->
+
+<?php include("fixed/whyfoot.php");?>
+
+<!----- FOOTER ENDS HERE //----->
+
+</div>
+</body>
+</html>
