64a9d804d2a35f8f273fb98d2807861d1b11acd2
jadegung <jadegung@gmail.com>
2015-12-05 15:56:19 -0500

    Create directions.php

---
+++ directions.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+<title>Welcome to Wing Hong Yip's Dragon Style Kung Fu and Tai Chi Club!</title>
+
+
+<link rel="stylesheet" href="fixed/whystory.css" type="text/css">
+<style type="text/css">
+<!--
+.Red {
+	color: #F00;
+}
+.yellow {
+	color: #FF0;
+}
+-->
+</style>
+<script language="JavaScript">
+<!--
+
+//Disable right mouse click Script
+//By Maximus (maximus@nsimail.com) w/ mods by DynamicDrive
+//For full source code, visit http://www.dynamicdrive.com
+
+var message="This image is copyright protected.";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+//-->
+</script>
+</head>
+<body bgcolor="#990000" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+
+<body>
+<div class="container">
+
+<!---- LOGO AND ADVERTISEMENTS BEGIN HERE //----->
+<?php include("fixed/why_bann.php"); ?>
+
+</div>
+
+<div class="about">
+<strong><center><h2 class=yellow>Kindly contact us for more information about the Club at info@yipsdragonstylekungfu.com</h2></center></strong>
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="footer">
+
+<!---- FOOTER BEGINS HERE //----->
+
+<?php include("fixed/whyfoot.php");?>
+
+<!----- FOOTER ENDS HERE //----->
+
+</div>
+</body>
+</html>
